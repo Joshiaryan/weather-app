7a4f31b52bdad3a5b89e4768bfea669109b052aa
aryan <aryanjoshi081@gmail.com>
2026-05-26 15:33:00 +0530

    first commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Weather.css'
-import search_icon from '../assets/search.png'
+import search from '../assets/search.png'
 
 const Weather = () => {
   return (
     <div className='weather'>
         <div className='Search-bar '>
             <input type="text" placeholder='Enter city name' />
-            <button><img src={search_icon} alt="search" /></button>
+            
+            <img src={search} alt="search" />
         </div>
     </div>
   )
